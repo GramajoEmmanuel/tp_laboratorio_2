0e663afc39ebf47686389bd39f973afe5d33f170
Emmanuel Gramajo <Emmanuel Gramajo>
2016-09-12 22:03:06 -0300

    commit

---
+++ Gramajo.Emmanuel.2C.TP01/Gramajo.Emmanuel.2C.TP01/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Gramajo.Emmanuel._2C.TP01
+{
+    class Program
+    {
+        /// <summary>
+        /// Entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
